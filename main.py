7b8aa5d4fd750d9301f074b311e0ecd5f5e005b4
Kilanko Daniel <danielkilanko14@gmail.com>
2026-07-04 00:20:08 +0100

    working

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
+posts : list[dict] = [
+    {
+       'id' : 1,
+       'title' : "first post",
+       'author' : "john doe",
+       'content' : "this is my first pose",
+    },
+    
+    {
+        'id' : 2,
+        'title' : "second post",
+        'author' : "jane derrick",
+        'content' : "this is my second post",
+    },
+]
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message" : "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return posts
+
+@app.post("/createposts")
+def create_post(payload : dict = Body(...)):
+    return {"new_post" : f'title : {payload['title']} content : {payload['content']} '}
+
