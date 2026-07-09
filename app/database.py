53ea27607b0cbf72ea62789bdb715f041b482df1
Kilanko Daniel <danielkilanko14@gmail.com>
2026-07-09 22:19:53 +0100

    working with orm

---
+++ app/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_pass=os.getenv('DB_pass')
+db_host=os.getenv('DB_host')
+db_user=os.getenv('DB_user')
+db=os.getenv('DB')
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{db_user}:{db_pass}@{db_host}/{db}'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
