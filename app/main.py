08898d2cdcd7c4ac1cd442007d9806bc83a22c1f
Kilanko Daniel <danielkilanko14@gmail.com>
2026-07-11 18:22:12 +0100

    working with orm

--- app/main.py
+++ app/main.py
@@ -6,13 +6,20 @@ from psycopg2.extras import RealDictCursor
 import os
 from dotenv import load_dotenv
 import time
-from app.database import engine
+from app.database import engine, SessionLocal
 
 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 load_dotenv()
