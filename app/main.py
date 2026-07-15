f5cca73b4608cca8fae610aaf150f89375d10b7f
Kilanko Daniel <danielkilanko14@gmail.com>
2026-07-15 10:15:50 +0100

    added user route and moving on to routers

--- app/main.py
+++ app/main.py
@@ -1,18 +1,9 @@
-from app import models, schemas
+from app import models, schemas, utilis
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import os
-from dotenv import load_dotenv
-import time
 from app.database import engine, get_db
 from sqlalchemy.orm import Session
 from typing import List
-from passlib.context import CryptContext
-from pwdlib import PasswordHash
 
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -115,14 +106,21 @@ def update_post(id: int, post: schemas.PostCreate, db : Session = Depends(get_db
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.User)  
 def create_user( user: schemas.UserCreate,     db: Session = Depends(get_db)):
     
-    # hashed_password =  pwd_context.hash(user.password[:72])
-    # user.password = hashed_password
-    password_hash = PasswordHash.recommended()
-    user.password = password_hash.hash(user.password)
+    password_hash = utilis.hash(user.password)
+    user.password = password_hash
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     
     return new_user
+
+@app.get("/users/{id}", response_model=schemas.User)
+def get_user(id : int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
     
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'user with the id of {id}, does not exist')
+    
+    return user
--- app/utilis.py
+++ app/utilis.py
@@ -1,1 +1,6 @@
 from pwdlib import PasswordHash
+
+def hash(password : str):
+    password_hash = PasswordHash.recommended()
+    user_password = password_hash.hash(password)
+    return user_password
