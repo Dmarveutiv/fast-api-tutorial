07dba1a6c521cbaac24d41f76de933841fcef608
Kilanko Daniel <danielkilanko14@gmail.com>
2026-07-16 16:58:28 +0100

    oauth

--- app/oauth2.py
+++ app/oauth2.py
@@ -3,6 +3,12 @@ from jwt.exceptions import InvalidTokenError
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
+from app.schemas import TokenData
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 load_dotenv()
@@ -24,3 +30,33 @@ def create_access_token(data : dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     
     return encoded_jwt
+
+def verify_access_token(token : str, credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )):
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm=ALGORITHM)
+        user_id : str = payload.get("user_id")
+    
+        if user_id is None:
+            raise credentials_exception
+    
+        token_data = TokenData(id= user_id)
+    except InvalidTokenError:
+        raise credentials_exception
+    
+    return token_data
+    
+def get_current_user(token : str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                            detail="Could not validate credentials",
+                                            headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    return verify_access_token(token, credentials_exception)
+        
+        
+    
