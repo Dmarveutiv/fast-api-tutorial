76c2d38b15a7bcc5d890231e724156f613f2c955
Kilanko Daniel <danielkilanko14@gmail.com>
2026-07-28 20:47:53 +0100

    rm engine in main

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from app.database import engine
 from .routers import post, user, auth, votes
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
