f3dc669194709352c13193f1525cb587df9a8358
Kilanko Daniel <danielkilanko14@gmail.com>
2026-07-28 20:40:20 +0100

    alembic migration

---
+++ alembic/versions/2ce86546d677_creating_posts_table.py
@@ -0,0 +1,31 @@
+"""creating posts table
+
+Revision ID: 2ce86546d677
+Revises: 
+Create Date: 2026-07-28 20:14:18.564504
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '2ce86546d677'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table('posts', sa.Column('id', sa.Integer(), nullable=False, primary_key=True),
+                              sa.Column('title', sa.String(), nullable=False),
+                              sa.Column('content', sa.String(), nullable=False))
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table('posts')
+    pass
