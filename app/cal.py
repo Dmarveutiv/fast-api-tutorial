c8babeed9280e38683724634509059c0a04def65
Kilanko Daniel <danielkilanko14@gmail.com>
2026-08-15 10:04:20 +0100

    testing classes obj

--- app/cal.py
+++ app/cal.py
@@ -1,6 +1,39 @@
+import random
+
 def add(num1: int , num2: int):
-    return num1 + num2 + 1
+    return num1 + num2 
 
 
 def substract(num1, num2):
     return num1 - num2
+
+
+
+
+class BankAccount():
+    def __init__(self, balance: int, user):
+        self.balance = balance
+        self.user = user
+        self.history = []
+        
+    def get_acct_no(self):
+        acct_no = "".join(random.choice("0123456789") for _ in range(10))
+        self.history.append(acct_no)
+        return f'for user: {self.user} acct no is :{acct_no}'
+    
+        
+    def deposit(self, amount: int):
+        if amount > 0:
+            self.balance+=amount
+            self.history.append(self.balance)
+            
+        return f'your new balance {self.balance}'
+    
+    def withdraw(self, amount: int):
+        if amount <= self.balance:
+            self.balance-=amount
+            self.history.append(self.balance)
+        return f'You withdrew {amount} and your new balance is {self.balance}'
+    
+    def check_balance(self):
+        return self.history
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,12 +1,18 @@
+import pytest
 from app.cal import add, substract
 
-def test_add():
+
+@pytest.mark.parametrize("num1, num2, expected" , argvalues=[
+    (4, 2, 6),
+    (3, 4, 7),
+    (2, 3, 5),
+])
+def test_add(num1, num2, expected):
     print('testing add func')
-    assert add(4, 2)== 6
+    assert add(num1, num2)== expected
     
      
 def test_substract():
     assert substract(5, 2) == 3
 
-test_add()
 test_substract()
