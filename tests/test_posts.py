12365660478f88fbe110d072934ee88b6c413eba
Kilanko Daniel <danielkilanko14@gmail.com>
2026-08-21 12:01:56 +0100

    testing get all posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,7 +1,18 @@
+from app import schemas
+
 def test_get_all_posts(authorized_client, put_posts):
     res = authorized_client.get('/posts/')
-    print(res.json())
+    # print(res.json())
     
-    assert res.status_code == 200
+    def validate(post):
+        return schemas.VotePost(**post)
+    posts_map = map(validate, res.json())
+    print(list(posts_map))
     
+    assert len(res.json()) == len(put_posts)
+    assert res.status_code == 200 
     
+def test_unauthorized_client_get_all_post(client):
+    res = client.get('/posts')
+    
+    assert res.status_code == 401
